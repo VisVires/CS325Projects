48b112eb40c78d65345a6f1e9a1e27163612801c
thrashek <thrashek@oregonstate.edu>
2016-01-17 10:05:06 -0800

    Python file
    Added file for project code

---
+++ Project1/project1.py
@@ -0,0 +1,21 @@
+'''
+Project 1 - Maximum Sum Subarray
+CS325 
+Group 11 - Karen Thrasher, William George, Kyle Livermore
+'''
+
+import timeit #Include for timing code
+
+
+def main():
+	print "Hello world!"
+
+
+main()
+
+'''
+Sample timing code:
+timer = timeit.Timer(stmt='fibIterative(500000)', setup='from __main__ import fibIterative')
+print timer.timeit(number=10)
+#Reference: http://www.dreamincode.net/forums/topic/288071-timeit-module/
+'''
