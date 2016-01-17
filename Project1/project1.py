75b3ba6c55980ca303b7cc9641edcdb246b65dbf
thrashek <thrashek@oregonstate.edu>
2016-01-17 10:27:30 -0800

    Function stubs added

--- Project1/project1.py
+++ Project1/project1.py
@@ -6,10 +6,53 @@ Group 11 - Karen Thrasher, William George, Kyle Livermore
 
 import timeit #Include for timing code
 
+#File input
+def fileInput():
+	print "File Input"
+
+
+#File output
+def fileOutput():
+	print "File Output"
+
+
+#Creates array of random numbers
+def randomArray():
+	print "Random array"
+
+
+#Algorithm 1 - Enumeration
+def a1():
+	print "Algorithm 1 - Enumeration"
+
+
+#Algorithm 2 - Better Enumeration
+def a2():
+	print "Algorithm 2 - Better Enumeration"
+
+
+#Algorithm 3 - Divide and Conquer
+def a3():
+	print "Algorithm 3 - Divide and Conquer"
+
+
+#Algorithm 4 - Linear-time
+def a4():
+	print "Algorithm 3 - Divide and Conquer"
+
 
 def main():
-	print "Hello world!"
 
+	fileInput()
+
+	randomArray()
+
+	a1()
+	a2()
+	a3()
+	a4()
+
+	fileOutput()
 
 main()
 
