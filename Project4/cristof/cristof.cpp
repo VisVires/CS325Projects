bc1a6c0eab9df9a4ae2643573a457c1e539260a4
William George <visviresfit@gmail.com>
2016-03-03 21:28:13 -0800

    added odd degree function

--- Project4/cristof/cristof.cpp
+++ Project4/cristof/cristof.cpp
@@ -1,20 +1,32 @@
 #include "cristof.h"
 
-cristof::cristof()
+cristof::cristof(int n)
 {
+    //create graph size of number of nodes
+    graph = new int*[n];
+    for (int i = 0; i < n; i++) {
+        //create multidimensional graph
+        graph[i] = new int[n];
+        //initialize graph
+        for (int j = 0; j < n; j++)
+            graph[i][j] = 0;
+	}
 
+	//adjacency list for mst T
+    mst = new vector<int> [n];
 }
 
 cristof::~cristof()
 {
-    //dtor
+    delete[] graph;
+    delete[] mst;
 }
 
 //http://www.geeksforgeeks.org/greedy-algorithms-set-5-prims-minimum-spanning-tree-mst-2/
 void cristof::primMST(int graph[V][V]){
 
     int currMst[V];     //holds MST
-    int key[V];         //
+    int key[V];         //holds current key
     bool setMst[V];     //set of vertices not in MST yet
 
     //initialize all keys to infinity and bool in set to false
@@ -40,6 +52,17 @@ void cristof::primMST(int graph[V][V]){
         }
 
     }
+
+    //move mst to mst matrix and build adjacency list
+    for (int start = 0; start < n; start++){
+        int last = currMst[start];
+        //if not root
+        if(last != -1){
+            //key each value to it's adjacent edge
+            mst[start].push_back(last);
+            mst[last].push_back(start);
+        }
+    }
 }
 
 //find vertex from those not yet in MST with min value
@@ -51,6 +74,13 @@ int cristof::minKey(int key[], bool setMst[]){
             min = key[v];
             min_index = v;
     return min_index;
+}
+
+void cristof::findOddDegree(){
+    for (int i = 0; i < nodes; i++){
+        if(())
+    }
+
 }
 
 void cristof::printMST(){
--- Project4/cristof/cristof.h
+++ Project4/cristof/cristof.h
@@ -7,12 +7,18 @@
 class cristof
 {
     private:
+        int n;
 
     public:
+        vector<int>odds;
+        vector<int> *mst;
+        int **graph;
+        int **tsp;
         cristof();
         ~cristof();
         void primMST(int graph[][]);
         int minKey(int key[], bool setMst[]);
+        void findOddDegree();
         void printMST();
 
 };
