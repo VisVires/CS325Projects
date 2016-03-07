ac953494f475d0574f467836281c02e54e8b67f8
thrashek <thrashek@oregonstate.edu>
2016-03-07 07:01:53 -0800

    Remove opt2 from libfunctions

--- Project4/2opt_3opt/2opt_3opt/opt2_opt3.cpp
+++ Project4/2opt_3opt/2opt_3opt/opt2_opt3.cpp
@@ -7,6 +7,53 @@
 #include <time.h>
 #include <limits>
 #include <algorithm>
+
+
+TSPtour opt2(TSPtour existing_tour, TSPadjMatrix adjM){
+    TSPtour new_tour;
+    int iteration = 0;
+    int best_distance = 0;
+    int new_distance = 0;
+
+    start_again:
+        best_distance = calcTourLen(existing_tour, adjM);
+        for (int i = 0; i < existing_tour.length - 1; i++) {
+            for (int k = i + 1; k < existing_tour.length; k++) {
+                new_tour = opt2Swap(existing_tour, i, k);
+                new_distance = calcTourLen(new_tour, adjM);
+                if (new_distance < best_distance) {
+                   existing_tour = new_tour;
+                   goto start_again;
+               }
+            }
+        }
+
+    //std::cout << "Opt2 end of function";
+}
+
+TSPtour opt2Swap(TSPtour route, int i, int k) {
+  TSPtour new_route = route;
+  int tempK = k;
+  //1. take route[1] to route[i-1] and add them in order to new_route
+  for (int j = 1; j < route[i-1]; j++){
+    //new_route[j] = route[j];
+  }
+
+  //2. take route[i] to route[k] and add them in reverse order to new_route
+  for(int j=i; j<k; j++){
+    //new_route[j] = route[tempK-1];
+    tempK--; 
+  }
+
+  //3. take route[k+1] to end and add them in order to new_route
+  for (int j = k+1; j < route.length; j++){
+    //new_route[j] = route[j];
+  }
+
+  return new_route;
+}
+
+
 /*
 def twoOpt(tour, adjMatrix):
     tour.append(tour[0])
@@ -54,7 +101,7 @@ TSPtour opt2(TSPtour existing_tour, TSPadjMatrix adjM){
        }
    }
 */
-
+/*
 TSPtour opt2(TSPtour existing_tour, TSPadjMatrix adjM){
     TSPtour new_tour;
     int iteration = 0;
@@ -98,15 +145,16 @@ TSPtour opt2Swap(TSPtour route, int i, int k) {
 
   return new_route;
 }
+*/
 
 
-
-
+/*
 TSPtour opt3(TSPadjMatrix adjM){
 
 
     cout << "Opt3 end of function";
 }
+*/
 
 /* Functions from other code */
 int calcTourLen(TSPtour tspTour, TSPadjMatrix adjM)
